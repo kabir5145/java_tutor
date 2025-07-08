d583189c8ee3ff4ac125d9dc75fc7a0cd96a8009
kabir5145 <kabir10112006@gmail.com>
2025-07-09 00:03:22 +0530

    Created a library management system.

---
+++ src/com/company/practice_set_15.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class practice_set_15 {
+    public static void main(String[] args) {
+        //question 1
+        ArrayList<String> students= new ArrayList<>();
+        students.add("kabir");
+        students.add("raghav");
+    }
+}
