94ca9dade431642083c0da9c639a826be0fc9fc5
kabir5145 <kabir10112006@gmail.com>
2025-07-09 11:12:46 +0530

    Starting some question practice

--- src/com/company/cwh_104_ex7.java
+++ src/com/company/cwh_104_ex7.java
@@ -1,64 +1,125 @@
 package com.company;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class cwh_104_ex7 {
-    interface library{
-       void book(String k);
-       void author(String n);
-       void issuedTo(String q);
-       void issuedOn(int d);
 
-    }
-    static class name implements library{//
-        @Override
-        public void book(String k) {
-            System.out.println(k);;
+    static class Book{//
+        private String name;
+        private String author;
+        private boolean issued = false;
+
+        public String getName() {
+            return name;
         }
 
-        @Override
-        public void author(String n) {
-            System.out.println(n);
+        public void setName(String name) {
+            this.name = name;
         }
 
-        @Override
-        public void issuedTo(String q) {
-            System.out.println(q);
+        public String getAuthor() {
+            return author;
         }
 
-        @Override
-        public void issuedOn(int d) {
-            System.out.println(d);
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public boolean isIssued() {
+            return issued;
+        }
+
+        public void setIssued(boolean issued) {
+            this.issued = issued;
         }
     }
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        /*
-        Create a library management system which is capable of  issuing books to the students.
-        Book should have info like:
-        1. Book name
-        2. Book author
-        3. Issued to
-        4. Issued on
-        user should be able to add books , return issued books ,issue books
-        assume that all the users are registered with their names in the central database
-         */
-        LocalDateTime lc = LocalDateTime.now();
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yy");
-        String o = lc.format(df);
-        name e = new name();
-        System.out.println("Name of the book is :");
-        e.book("rich dad poor dad");
-        System.out.println("name of the author is:");
-        String n = sc.next();
-        System.out.println("Issued to :");
-        String  q = sc.next();
-        System.out.println("book issued on:");
-        System.out.println(o);
-        System.out.println("Thank you for issuing the book from us!");
+
+        List<Book> availableBooks = new ArrayList<>();
+        Book book1 = new Book();
+
+        book1.setName("Sherlock Holmes");
+        book1.setAuthor("Arthur Conan Doyle");
+        availableBooks.add(book1);
+
+        Book book2 = new Book();
+
+        book2.setName("Rich Dad Poor Dad");
+        book2.setAuthor("Baba Ji");
+        availableBooks.add(book2);
+
+
+        while (true) {
+            System.out.println("1. Add Book");
+            System.out.println("2. Remove Book");
+            System.out.println("3. Issue Book");
+            System.out.println("4. Return Book");
+            System.out.println("5. Show all Books");
+            System.out.println("6. Exit");
+            System.out.print("Select one option: ");
+
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1: // Add Book
+                    Scanner bookScanner = new Scanner(System.in);
+
+                    System.out.print("Enter Book Name: "); // use print, not println
+                    String bookName = bookScanner.nextLine();
+
+                    System.out.print("Enter Author: "); // use print again
+                    String author = bookScanner.nextLine();
+
+                    Book book = new Book();
+                    book.setName(bookName);
+                    book.setAuthor(author);
+                    availableBooks.add(book);
+                    break;
+                case 2: // Remove Book
+                    Scanner removeBookScanner = new Scanner(System.in);
+
+                    System.out.print("Enter Book Name: "); // use print, not println
+                    String name = removeBookScanner.nextLine();
+                    int bookToRemoveIndex = -1;
+                    for (int i = 0; i < availableBooks.size(); i++) {
+                        if (availableBooks.get(i).getName().equals(name)) {
+                            bookToRemoveIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (bookToRemoveIndex == -1) {
+                        System.out.println("Book not found");
+                    }else {
+                        availableBooks.remove(bookToRemoveIndex);
+                        System.out.println("Book removed");
+                    }
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Available Books:");
+                    for (Book b : availableBooks) {
+                        System.out.println("Name: " + b.getName());
+                        System.out.println("Author: " + b.getAuthor());
+                        System.out.println("Issued: " + b.isIssued());
+                    }
+                    break;
+                case 6:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
     }
 }
--- src/com/company/practice_set_15.java
+++ src/com/company/practice_set_15.java
@@ -3,11 +3,49 @@ package com.company;
 import java.util.ArrayList;
 
 public class practice_set_15 {
+
+
+    /*
+     *       *
+     *      * *
+     *     * * *
+     *
+     * */
     public static void main(String[] args) {
-        //question 1
-        ArrayList<String> students= new ArrayList<>();
-        students.add("kabir");
-        students.add("raghav");
+//        int n = 5;
+//
+//        for (int i = 0; i <= n; i++) {
+//            for (int j = 0; j < n - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k < i; k++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = n; i > 0; i--) {
+//            for (int j = 0; j < n - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k < i; k++) {
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+
+//        int n = 10;
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < 15; i++) {
+            System.out.print((a+b) +", ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
 
     }
 }
