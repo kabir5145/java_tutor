0b3aa890e86be60d35438bc84b7d5655aab6018d
kabir5145 <kabir10112006@gmail.com>
2025-07-12 11:09:10 +0530

    learning tags in java

---
+++ src/com/company/cwh_106_javadoc.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * this class is to demonstrate what javadoc is and how it is used in the java industry
+ * @author kabir(CodeWithHarry)
+ * @version 0.1
+ * @see <a href = "https://download.java.net/java/GA/jdk14/docs/api/index.html" target = _blank></a>java docs
+ * @since 2006
+ **/
+
+
+public class cwh_106_javadoc {
+
+    public void add(int a, int b){
+        System.out.println(" the sum is : "+a+b);
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
